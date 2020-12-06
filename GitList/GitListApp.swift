83afb7e398f4d8e2cb0dd1da23299efd0324306c
Vitalii Yremchuk <montybanks.mb@gmail.com>
2020-12-07 01:37:43 +0200

    initial commit

---
+++ GitList/GitListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitListApp.swift
+//  GitList
+//
+//  Created by Vitalii Yaremchuk on 07.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct GitListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
