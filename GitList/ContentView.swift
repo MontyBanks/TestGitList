3d91246780d92608d91e4ee2f02f34773b0b6e5f
Vitalii Yremchuk <montybanks.mb@gmail.com>
2020-12-07 04:12:46 +0200

    runing in 1 thread. Im realy sorry for that crapy code, but it works

--- GitList/ContentView.swift
+++ GitList/ContentView.swift
@@ -6,13 +6,76 @@
 //
 
 import SwiftUI
+import Alamofire
+
 
 struct ContentView: View {
+    @ObservedObject var observed = Observer()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        List(observed.repoItems){ i in
+            HStack{Text(i.name)
+                Spacer()
+                Text(String(i.stars))
+                }
+            }
+        
     }
+    func addRepo(){
+            observed.fetchItems()
+        }
 }
+
+
+
+struct Repo: Identifiable{
+    let id: Int
+    let name: String
+    let stars: Int
+    
+}
+
+
+class Observer : ObservableObject{
+    @Published var repoItems = [Repo]()
+
+    init() {
+        fetchItems()
+    }
+    
+    func fetchItems() {
+        // 1
+        AF.request("https://api.github.com/search/repositories?q=most+ranked")
+            .responseJSON{
+                            response in
+                switch response.result {
+                case let .success(value):
+                    let json = value
+                                if  (json as? [String : AnyObject]) != nil{
+                                    if let dictionaryArray = json as? Dictionary<String, AnyObject?> {
+                                        let jsonArray = dictionaryArray["items"]
+
+                                        if let jsonArray = jsonArray as? Array<Dictionary<String, AnyObject?>>{
+                                            for i in 0..<jsonArray.count{
+                                                let json = jsonArray[i]
+                                                if let id = json["id"] as? Int, let name = json["name"] as? String, let stars = json["stargazers_count"] as? Int{
+                                                    self.repoItems.append(Repo(id: id, name: name, stars: stars))
+                                                }
+                                            }
+                                        }
+                                    }
+                                }case let .failure(error):
+                                    print(error)
+                                }
+                            }
+                
+            }
+      }
+
+    
+    
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ GitList/Model/RepoModel.swift
@@ -0,0 +1,18 @@
+//
+//  RepoModel.swift
+//  GitList
+//
+//  Created by Vitalii Yaremchuk on 07.12.2020.
+//
+
+//import Foundation
+//
+//struct Repo: Decodable{
+//    let name: String
+//    let stars: Int
+//
+//    enum CodingKeys: String, CodingKey {
+//        case name = "name"
+//        case stars = "stargazers_count"
+//    }
+//}
